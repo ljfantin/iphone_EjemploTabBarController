889c99ecfec648ed5e2ab2d9c2dd12101bbe7386
Leandro Fantin <lfantin@mac-lfantin.local>
2014-07-15 11:30:55 -0300

    Initial Commit

---
+++ EjemploTabBarControllerTests/EjemploTabBarControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  EjemploTabBarControllerTests.m
+//  EjemploTabBarControllerTests
+//
+//  Created by Leandro Fantin on 15/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EjemploTabBarControllerTests : XCTestCase
+
+@end
+
+@implementation EjemploTabBarControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
